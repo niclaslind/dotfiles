a49fc3386750a3f194f91ba09e6b10560bc368f7
Niclas <niclas.lind93@gmail.com>
2022-05-02 21:46:36 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,17 +2,14 @@
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 
-export HTTP_PROXY=http://127.0.0.1:1087
-export HTTPS_PROXY=http://127.0.0.1:1087
-export NO_PROXY=localhost,127.0.0.1
-
 # Useful alias
 alias please='sudo'
 alias ls='exa'
 alias cat='batcat' 
 alias ...='cd ../../'
 alias ....='cd ../../../' 
-
+alias .....='cd ../../../../'
+alias ......='cd ../../../../../'
 
 abbr g "git"
 abbr gst "git status" 
@@ -31,8 +28,11 @@ set PATH /usr/local/go/bin $PATH
 set GOROOT /usr/local/go
 set GOPATH $HOME"/go/bin"
 
+# CSP DEV
+export CSP_ENV=docker
+
 # Add java to path
-set JAVA_HOME /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre
+set JAVA_HOME /home/nlind1/opt/android-studio/jre
 
 # Add android to path
 set ANDROID_HOME $HOME"/Android/Sdk"
@@ -40,11 +40,12 @@ set PATH $ANDROID_HOME/tools $PATH
 set PATH $ANDROID_HOME/tools/bin $PATH
 set PATH $ANDROID_HOME/platform-tools $PATH 
 set PATH $ANDROID_HOME/emulator $PATH
+set ANDROID_SDK_ROOT $HOME"/ANDROID/Sdk"
 
 # Add esp32-xtensa to path
 set PATH $HOME"/esp/xtensa-esp32-elf/bin" $PATH
 
 # Add platformIO to path
-set PATH $HOME".platformio/penv/bin/" $PATH
+set PATH $HOME"/.platformio/penv/bin" $PATH
 
 starship init fish | source
